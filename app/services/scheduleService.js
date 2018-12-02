eac418262734e1a9f5bd9c8cbda3d86d9bb4e0c3
Bryant, Charles <c3bryant@ucsd.edu>
2018-12-02 09:54:26 -0800

    Update schedule

--- app/services/scheduleService.js
+++ app/services/scheduleService.js
@@ -6,24 +6,13 @@ import logger from '../util/logger'
 const ScheduleService = {
 	* FetchSchedule(term) {
 		const data = [],
-			UN_API_URL = AppSettings.ACADEMIC_HISTORY_API_URL + '?academic_level=UN&term_code=' + term,
-			GR_API_URL = AppSettings.ACADEMIC_HISTORY_API_URL + '?academic_level=GR&term_code=' + term
+			UN_API_URL = AppSettings.ACADEMIC_HISTORY_API_URL + '?academic_level=UN&term_code=' + term
 
 		// Query api for undergrad classes
 		try {
 			const undergrad = JSON.parse(yield authorizedFetch(UN_API_URL))
 			if (undergrad.data && Array.isArray(undergrad.data)) {
 				data.push(...undergrad.data)
-			}
-		} catch (err) {
-			logger.trackException(err, false)
-		}
-
-		// Query api for graduate classes
-		try {
-			const grad = JSON.parse(yield authorizedFetch(GR_API_URL))
-			if (grad.data && Array.isArray(grad.data)) {
-				data.push(...grad.data)
 			}
 		} catch (err) {
 			logger.trackException(err, false)
